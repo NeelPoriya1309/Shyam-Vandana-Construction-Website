596eb5bcecfb10c24faeab4a6d03983bd85e49b6
Neel Poriya <neelporiya1309@gmail.com>
2022-12-18 14:10:16 +0530

    Refactoring Components

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -2,18 +2,24 @@ import styles from './ContactUs.module.css';
 
 function ContactUs() {
   return (
-    <div className={styles['container']}>
-      <div className={styles['card']}>
-        <img src="https://picsum.photos/300/300"></img>
-        <p className={styles['contact-name']}>Person 1</p>
-        <p className={styles['contact-mobile']}>+91-123456789</p>
+    <section id="contact" className={styles['contact']}>
+      <div className={styles['title']}>
+        <h2>Contact Us</h2>
+        <div className={styles['bar']}></div>
       </div>
-      <div className={styles['card']}>
-        <img src="https://picsum.photos/300/300"></img>
-        <p className={styles['contact-name']}>Person 2</p>
-        <p className={styles['contact-mobile']}>+91-123456789</p>
+      <div className={styles['container']}>
+        <div className={styles['card']}>
+          <img src="https://picsum.photos/300/300"></img>
+          <p className={styles['contact-name']}>Person 1</p>
+          <p className={styles['contact-mobile']}>+91-123456789</p>
+        </div>
+        <div className={styles['card']}>
+          <img src="https://picsum.photos/300/300"></img>
+          <p className={styles['contact-name']}>Person 2</p>
+          <p className={styles['contact-mobile']}>+91-123456789</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
